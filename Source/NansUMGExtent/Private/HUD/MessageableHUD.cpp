bc707feff4d74befe107846dd8962e44e8739794
Nans Pellicari <nans.pellicari@gmail.com>
2021-02-21 15:09:52 +0100

    fix: a debug message for MessageableHUD

--- Source/NansUMGExtent/Private/HUD/MessageableHUD.cpp
+++ Source/NansUMGExtent/Private/HUD/MessageableHUD.cpp
@@ -1,4 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2020-present Nans Pellicari (nans.pellicari@gmail.com).
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "HUD/MessageableHUD.h"
 #include "Widget/MessageableWidget.h"
@@ -25,12 +36,15 @@ void AMessageableHUD::BeginPlay()
 
 void AMessageableHUD::WhenEndDisplayMessage()
 {
-	if (bDebug == true) UE_LOG(
-		LogTemp,
-		Log,
-		TEXT("Message \"%s\" should not be displayed anymore"),
-		*CurrentMessage.Message.ToString()
-	);
+	if (bDebug == true && !CurrentMessage.Message.IsEmpty())
+	{
+		UE_LOG(
+			LogTemp,
+			Log,
+			TEXT("Message \"%s\" should not be displayed anymore"),
+			*CurrentMessage.Message.ToString()
+		);
+	}
 
 	CurrentMessage.Clear();
 	if (!MessagesQueues.IsEmpty())
